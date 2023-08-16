ef5de47d816521b43681b61b3f7a212e6942f706
asg1584 <86628659+asg1584@users.noreply.github.com>
2023-08-17 00:05:42 +0200

    Component Study
    I added Class Components and imported into JS file

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import logo from './logo.svg';
+
+import User from "./components/User";
 import './App.css';
-
 function App() {
-  const test = 34;
-  const first = 24;
-  const second = 14;
-  const last = 12;
 
-  const isAuth = false;
   return (
     <div className="App">
-     <h1>It takes approximately 3 hours to say, but Hello React</h1>
-     <h4>{1 + 1} </h4>
-     <h4>{"murat".toUpperCase()}</h4>
-     <h4>{test}</h4>
-     <div>
-      {
-        isAuth ? <p>User is registered</p>
-        : null
+      <h4> App Component</h4>
+      <User/>
+      
 
-      }
-
-     </div>
     </div>
   );
 }
