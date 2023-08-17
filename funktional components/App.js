d1c9e2bcdf326494c789a817ad912698ea31112c
asg1584 <86628659+asg1584@users.noreply.github.com>
2023-08-17 23:05:02 +0200

    Functional Components
    I used Functional Components in this files

---
+++ funktional components/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/navbar';
+
+import './App.css';
+function App() {
+
+  return (
+    <div className="container">
+      <Navbar />
+
+     
+      
+
+    </div>
+  );
+}
+
+export default App;
